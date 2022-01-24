5ef667c231dccff6513c4e6a7579757e461b7885
unknown <alihassanuog50@gmail.com>
2022-01-24 23:59:34 +0500

    Basic Setup

--- db.js
+++ db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-let mongoURL = 'mongodb+srv://ali:12345@nodeandexpress-projects.gnvwa.mongodb.net/hotel-mern';
-
 mongoose.connect(process.env.URI, {
     useUnifiedTopology: true, useNewUrlParser: true
 });
