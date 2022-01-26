20ab7d09c9bdb90061fd8d5cbf2db1160194e02b
unknown <alihassanuog50@gmail.com>
2022-01-27 01:46:36 +0500

    HomeScreen and Modal Popup

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,18 +3,18 @@
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/register">Register</a>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/register">Register</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/login">Login</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/login">Login</a>
                             </li>
                         </ul>
                     </div>
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const dbConfig = require('./db');
+require('./db');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
